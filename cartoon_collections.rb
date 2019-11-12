d8cdca56eb1fbc39ce7950b59c4fac28e082bdce
Ben Miriello <benmiriello@gmail.com>
2019-11-12 17:03:29 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,5 +14,5 @@ end
 def find_the_cheese(input)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  input.detect{ |n| n == cheese_types.any}
+  input.detect{ |n| n == "cheddar" || "gouda" || "camembert"}
 end
