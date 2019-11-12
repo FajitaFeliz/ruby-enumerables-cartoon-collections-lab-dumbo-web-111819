90f1253464654ee099eb6f50d665ceeee75f9b00
Ben Miriello <benmiriello@gmail.com>
2019-11-12 16:59:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet(input)
 end
 
 def long_planeteer_calls(input)
-  input.any?{ |n| n.length >= 4 }
+  input.any?{ |n| n.length >= 3 }
 end
 
 def find_the_cheese# code an argument here
