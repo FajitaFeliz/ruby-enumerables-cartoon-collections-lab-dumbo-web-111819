ba4567f0585b9884d672acac845a01f49b706ea5
Ben Miriello <benmiriello@gmail.com>
2019-11-12 17:02:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,8 @@ def long_planeteer_calls(input)
   input.any?{ |n| n.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(input)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  input.any{ |n| n == cheese_types.any}
 end
